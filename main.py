46702f6d6de6e3fd1ecc042245c8f0e894855689
Kirill <kirilla223@gmail.com>
2023-05-04 14:38:55 +0300

    Change second dialog window

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 
+class CreateProjectDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Создать проект")
+
+        # Создаем 4 поля для ввода данных
+        self.line_edit_1 = QtWidgets.QLineEdit(self)
+        self.line_edit_2 = QtWidgets.QLineEdit(self)
+        self.line_edit_3 = QtWidgets.QLineEdit(self)
+        self.line_edit_4 = QtWidgets.QLineEdit(self)
+
+        # Создаем кнопки для сохранения и отмены
+        self.button_save = QtWidgets.QPushButton("Сохранить", self)
+        self.button_cancel = QtWidgets.QPushButton("Отмена", self)
+
+        # Устанавливаем лейаут для расположения элементов на окне
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.line_edit_1)
+        layout.addWidget(self.line_edit_2)
+        layout.addWidget(self.line_edit_3)
+        layout.addWidget(self.line_edit_4)
+        layout.addWidget(self.button_save)
+        layout.addWidget(self.button_cancel)
+
+        # Устанавливаем обработчики событий для кнопок
+        self.button_save.clicked.connect(self.save_clicked)
+        self.button_cancel.clicked.connect(self.reject)
+
+    def save_clicked(self):
+        # Сохраняем данные из полей в переменные
+        name = self.line_edit_1.text()
+        date = self.line_edit_2.text()
+        author = self.line_edit_3.text()
+        description = self.line_edit_4.text()
+
+        # Передаем названия колонок в таблице
+        column_names = ('id', name, author, description)
+
+        # Закрываем окно
+        self.accept()
+
 
 class Ui_Dialog(object):
     def __init__(self):
@@ -38,15 +79,28 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.comboBox.setItemText(0, _translate("Dialog", "Создать проект"))
-        self.comboBox.setItemText(1, _translate("Dialog", "Открыть проект"))
+        self.comboBox.setItemText(0, _translate("Dialog", "Список функций"))
+        self.comboBox.setItemText(1, _translate("Dialog", "Создать проект"))
         self.comboBox.setItemText(2, _translate("Dialog", "Сохранить проект"))
+        self.comboBox.setItemText(3, _translate("Dialog", "Открыть проект"))
         self.buttonSave.setText(_translate("Dialog", "OK"))
         self.buttonCancel.setText(_translate("Dialog", "Cancel"))
 
     def selectionChanged(self, index):
         # сохраняем выбранное значение
         self.selectedValue = self.comboBox.currentText()
+
+        # проверяем, было ли выбрано "Создать проект"
+        if self.selectedValue == "Создать проект":
+            # Создаем новое окно для ввода данных
+            create_project_dialog = CreateProjectDialog()
+
+            # Показываем окно и ждем, пока пользователь введет данные
+            if create_project_dialog.exec() == QtWidgets.QDialog.Accepted:
+                # Если пользователь нажал "Сохранить", то сохраняем данные
+                # и продолжаем выполнение программы
+                column_names = create_project_dialog.column_names
+                print(column_names)
 
     def saveClicked(self):
         if self.selectedValue:
