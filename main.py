7a7e479a2403121271016a63b0bf8b8dd85796a3
Kirill <kirilla223@gmail.com>
2023-05-10 09:57:19 +0300

    Changed general file - "main.py" Added config.json file

--- main.py
+++ main.py
@@ -1,118 +1,79 @@
+
+
 from PyQt6 import QtCore, QtGui, QtWidgets
-
-class CreateProjectDialog(QtWidgets.QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle("Создать проект")
-
-        # Создаем 4 поля для ввода данных
-        self.line_edit_1 = QtWidgets.QLineEdit(self)
-        self.line_edit_2 = QtWidgets.QLineEdit(self)
-        self.line_edit_3 = QtWidgets.QLineEdit(self)
-        self.line_edit_4 = QtWidgets.QLineEdit(self)
-
-        # Создаем кнопки для сохранения и отмены
-        self.button_save = QtWidgets.QPushButton("Сохранить", self)
-        self.button_cancel = QtWidgets.QPushButton("Отмена", self)
-
-        # Устанавливаем лейаут для расположения элементов на окне
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(self.line_edit_1)
-        layout.addWidget(self.line_edit_2)
-        layout.addWidget(self.line_edit_3)
-        layout.addWidget(self.line_edit_4)
-        layout.addWidget(self.button_save)
-        layout.addWidget(self.button_cancel)
-
-        # Устанавливаем обработчики событий для кнопок
-        self.button_save.clicked.connect(self.save_clicked)
-        self.button_cancel.clicked.connect(self.reject)
-
-    def save_clicked(self):
-        # Сохраняем данные из полей в переменные
-        name = self.line_edit_1.text()
-        date = self.line_edit_2.text()
-        author = self.line_edit_3.text()
-        description = self.line_edit_4.text()
-
-        # Передаем названия колонок в таблице
-        column_names = ('id', name, author, description)
-
-        # Закрываем окно
-        self.accept()
+from PyQt6.QtWidgets import QFileDialog
+import os
+import shutil
 
 
-class Ui_Dialog(object):
-    def __init__(self):
-        self.buttonCancel = None
-        self.buttonSave = None
-        self.comboBox = None
-        self.selectedValue = None
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textBrowser = QtWidgets.QTextBrowser(parent=self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(90, 70, 461, 291))
+        self.textBrowser.setObjectName("textBrowser")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 26))
+        self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(parent=self.menubar)
+        self.menu.setObjectName("menu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.action = QtGui.QAction(parent=MainWindow)
+        self.action.setObjectName("action")
+        self.action.triggered.connect(self.open_folder_dialog)
+        self.action_2 = QtGui.QAction(parent=MainWindow)
+        self.action_2.setObjectName("action_2")
+        #self.action_2.triggered.connect(self.open_file_dialog)
+        self.action_4 = QtGui.QAction(parent=MainWindow)
+        self.action_4.setObjectName("action_4")
+        self.action_6 = QtGui.QAction(parent=MainWindow)
+        self.action_6.setObjectName("action_6")
+        self.menu.addAction(self.action)
+        self.menu.addAction(self.action_2)
+        self.menu.addSeparator()
+        self.menu.addAction(self.action_4)
+        self.menu.addSeparator()
+        self.menu.addAction(self.action_6)
+        self.menubar.addAction(self.menu.menuAction())
 
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
-        Dialog.setStyleSheet("")
-        self.comboBox = QtWidgets.QComboBox(parent=Dialog)
-        self.comboBox.setGeometry(QtCore.QRect(140, 90, 121, 21))
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.buttonSave = QtWidgets.QPushButton(parent=Dialog)
-        self.buttonSave.setGeometry(QtCore.QRect(210, 230, 75, 24))
-        self.buttonSave.setObjectName("buttonSave")
-        self.buttonCancel = QtWidgets.QPushButton(parent=Dialog)
-        self.buttonCancel.setGeometry(QtCore.QRect(290, 230, 75, 24))
-        self.buttonCancel.setObjectName("buttonCancel")
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+    def open_folder_dialog(self):
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.FileMode.Directory)  # Установка режима выбора папки
+        folder_path = dialog.getExistingDirectory(None, "Выберите папку", QtCore.QDir.homePath())
+        new_folder_path = os.path.join(folder_path, "hello world")
+        os.mkdir(new_folder_path)
+        new_json_path = os.path.join(new_folder_path, "config.json")
+        existing_json_path = "C:/Users/Kirill/PycharmProjects/GraphPyQT/config.json"
+        shutil.copyfile(existing_json_path, new_json_path)
+        # Дальнейшая обработка выбранной папки
+        if folder_path:
+            print("Сохранили конфиг в папку:", folder_path)
 
-        # устанавливаем обработчики событий для кнопок
-        self.buttonSave.clicked.connect(self.saveClicked)
-        self.buttonSave.clicked.connect(Dialog.close)
-        self.buttonCancel.clicked.connect(Dialog.close)
-        self.comboBox.currentIndexChanged.connect(self.selectionChanged)
-
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.comboBox.setItemText(0, _translate("Dialog", "Список функций"))
-        self.comboBox.setItemText(1, _translate("Dialog", "Создать проект"))
-        self.comboBox.setItemText(2, _translate("Dialog", "Сохранить проект"))
-        self.comboBox.setItemText(3, _translate("Dialog", "Открыть проект"))
-        self.buttonSave.setText(_translate("Dialog", "OK"))
-        self.buttonCancel.setText(_translate("Dialog", "Cancel"))
-
-    def selectionChanged(self, index):
-        # сохраняем выбранное значение
-        self.selectedValue = self.comboBox.currentText()
-
-        # проверяем, было ли выбрано "Создать проект"
-        if self.selectedValue == "Создать проект":
-            # Создаем новое окно для ввода данных
-            create_project_dialog = CreateProjectDialog()
-
-            # Показываем окно и ждем, пока пользователь введет данные
-            if create_project_dialog.exec() == QtWidgets.QDialog.Accepted:
-                # Если пользователь нажал "Сохранить", то сохраняем данные
-                # и продолжаем выполнение программы
-                column_names = create_project_dialog.column_names
-                print(column_names)
-
-    def saveClicked(self):
-        if self.selectedValue:
-            print(self.selectedValue)
-            QtWidgets.QDialog().done(0)
+        MainWindow.setWindowTitle(_translate("MainWindow", "Graph Editor (v.0.1)"))
+        self.menu.setTitle(_translate("MainWindow", "Файл"))
+        self.action.setText(_translate("MainWindow", "Новый проект"))
+        self.action_2.setText(_translate("MainWindow", "Открыть проект"))
+        self.action_4.setText(_translate("MainWindow", "Сохранить"))
+        self.action_6.setText(_translate("MainWindow", "Выход"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec())
